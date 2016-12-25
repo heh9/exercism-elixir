a8952c6e2461d727a96d174ce2ac08b4d5f20d1e
Vladimir Mihai <vladimir.iacob@everymatrix.com>
2016-12-26 00:50:09 +0200

    solved space age problem

--- space-age/space_age.exs
+++ space-age/space_age.exs
@@ -8,6 +8,18 @@ defmodule SpaceAge do
   """
   @spec age_on(planet, pos_integer) :: float
   def age_on(planet, seconds) do
+    earth_seconds = 365.25 * 24 * 60 * 60
+    ages = %{
+      :mercury => 0.2408467,
+      :venus => 0.61519726,
+      :earth => 1,
+      :mars => 1.8808158,
+      :jupiter => 11.862615,
+      :saturn => 29.447498,
+      :uranus => 84.016846,
+      :neptune => 164.79132
+    }
 
+    (seconds / (ages[planet] * earth_seconds))
   end
 end
