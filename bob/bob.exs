dffcd7e1698d8be796b09cede0de5006f2ea8d7f
Vladimir Mihai <vladimir.iacob@everymatrix.com>
2016-12-28 22:15:29 +0200

    add solution to bob problem

--- bob/bob.exs
+++ bob/bob.exs
@@ -1,8 +1,20 @@
 defmodule Bob do
   def hey(input) do
-    cond do
-        true -> raise "Your implementation goes here"
+    string_input = input |> to_string
 
+    isShouting = fn (string) ->
+      Regex.match?(~r/\p{Lu}/, string) && String.upcase(string) == string
+    end
+
+    cond do
+      String.ends_with?(string_input, "?") ->
+        "Sure."
+      isShouting.(string_input) ->
+        "Whoa, chill out!"
+      !Regex.match?(~r/[\d+\w+]/, string_input) ->
+        "Fine. Be that way!"
+      true ->
+        "Whatever."
     end
   end
 end
